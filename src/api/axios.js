8b706cbaf2aeda102a1ab293f9374089cb6a3e67
pial <pialmahmud80@gmail.com>
2026-04-04 00:50:39 +0600

    Fix API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "@/router";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
